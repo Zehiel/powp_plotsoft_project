20b2cd6bb31674da3a213877e8fca159460e7e3a
Andrzej Borzecki <andrew.borzecki@gmail.com>
2017-06-08 01:20:32 +0200

    Added basic drawing functionality to drawer

---
+++ src/main/java/edu/iis/powp/events/predefine/SelectSaveCustomCommandOptionListener.java
@@ -0,0 +1,16 @@
+package edu.iis.powp.events.predefine;
+
+import edu.iis.powp.appext.FeaturesManager;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by grusz on 08.06.2017.
+ */
+public class SelectSaveCustomCommandOptionListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //FeaturesManager.drawerController().saveCustomCommand();
+    }
+}
