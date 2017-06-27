f934afc89475b7567c38e9578ef9de614368865e
Grzegorz Banach <19409@edu.p.lodz.pl>
2017-06-27 23:02:10 +0200

    forgot to add abstract command listener

---
+++ src/test/java/edu/iis/powp/events/AbstractCommandListener.java
@@ -0,0 +1,16 @@
+package edu.iis.powp.events;
+
+import edu.iis.powp.app.Application;
+import edu.iis.powp.command.IPlotterCommand;
+
+
+/**
+ * Created by user on 2017-06-27.
+ */
+public abstract class AbstractCommandListener {
+    protected Application context;
+
+    public AbstractCommandListener(Application context) {
+        this.context = context;
+    }
+}
